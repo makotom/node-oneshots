9ae25d0ecf5e051a698d01ca87dfd1c7cc293785
Makoto Mizukami <comm@makotom.org>
2014-07-09 12:49:51 +0900

    Copyright notes EOL conversion

--- core.js
+++ core.js
@@ -1,3 +1,5 @@
+// Copyright (C) 2014 Makoto Mizukami
+
 (function () {
 	"use strict";
 
--- http_status.js
+++ http_status.js
@@ -1,3 +1,5 @@
+// Copyright (C) 2014 Makoto Mizukami
+
 (function () {
 	var registered = exports.IANAStatuses = [];
 
